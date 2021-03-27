67d608d07d6c05a6099bea330218fd5d9c108436
Denys <denys.kovylin@temy.co>
2021-03-27 17:48:18 +0200

    Refactor SplashActivity to SplashFragment

---
+++ app/src/main/java/com/temy/coffeeshop/navigation/view/AppActivity.kt
@@ -0,0 +1,15 @@
+package com.temy.coffeeshop.navigation.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.temy.coffeeshop.R
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+}
